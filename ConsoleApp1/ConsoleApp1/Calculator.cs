7a32c7c2bc4a111c6daed096a5935f5103e18b9f
Thomas Thorin <thomas.thorin@gmail.com>
2021-11-01 14:44:23 +0100

    better reset

--- ConsoleApp1/ConsoleApp1/Calculator.cs
+++ ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -13,28 +13,34 @@
             double num1 = 0;
             double num2 = 0;
             bool dontExit = true;
+            bool firstTime = true;
 
             Console.WriteLine("Ange c som räknesätt för att nollställa eller x för att avsluta");
             while (dontExit)
             {
-                if (num1 == 0)
+                if (firstTime)
                 {
                     Console.Write("Skriv tal 1: ");
                     Double.TryParse(Console.ReadLine(), out num1);
+                    firstTime = false;
                 }
                 else Console.Write($"Tal 1 är: {num1}\n");
                 Console.Write("Ange räknesätt: ");
                 string thingToDo = Console.ReadLine();
                 if (thingToDo != "x" && thingToDo != "c")
                 {
-                    Console.Write("Skriv tal 2:");
+                    Console.Write("Skriv tal 2: ");
                     Double.TryParse(Console.ReadLine(), out num2);
                 }
                 if (thingToDo == "+") num1 = Add(num1, num2);
                 else if (thingToDo == "-") num1 = Subtract(num1, num2);
                 else if (thingToDo == "/") num1 = Divide(num1, num2);
                 else if (thingToDo == "*") num1 = Multiply(num1, num2);
-                else if (thingToDo == "c") num1 = 0;
+                else if (thingToDo == "c")
+                {
+                    num1 = 0;
+                    firstTime = true;
+                }
                 else if (thingToDo == "x") dontExit = true;
 
             }
