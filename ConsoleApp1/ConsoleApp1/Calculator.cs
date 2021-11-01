40ce2c35a112cf2a5e999b1da23b6b747f51d2d0
Thomas Thorin <thomas.thorin@gmail.com>
2021-11-01 13:55:18 +0100

    added Calculator

---
+++ ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Calculator
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
