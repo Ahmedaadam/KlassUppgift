dc6df708e7af1785034d335f254633e647a0bb28
Thomas Thorin <thomas.thorin@gmail.com>
2021-11-01 14:26:12 +0100

    all done?

--- ConsoleApp1/ConsoleApp1/Calculator.cs
+++ ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -12,14 +12,25 @@
         {
             double num1 = 0;
             double num2;
+            bool dontExit = true;
 
+            while(dontExit)
             if (num1 != 0)
             {
-                Console.WriteLine("Skriv tal 1:");
+                Console.Write("Skriv tal 1: ");
                 Double.TryParse(Console.ReadLine(), out num1);
             }
-            Console.WriteLine("Skriv tal 2:");
+            else Console.Write($"Tal 1 är: {num1}");
+            Console.Write("Skriv tal 2:");
+            string thingToDo = Console.ReadLine();
             Double.TryParse(Console.ReadLine(), out num2);
+
+            if (thingToDo == "+") Add(num1, num2);
+            else if (thingToDo == "-") Subtract(num1, num2);
+            else if (thingToDo == "/") Divide(num1, num2);
+            else if (thingToDo == "*") Multiply(num1, num2);
+            else if (thingToDo == "c") num1 = 0;
+            else if (thingToDo == "x") dontExit = true;
         }
         private static double Add(double a, double b)
         {
